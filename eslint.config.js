36e1b3a91af2945dde8e5503663d51acaef89c16
NindroidA <andrew@larrycurtis.com>
2024-06-02 09:45:34 -0500

    initial colors

---
+++ eslint.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+    ignores: [
+        ".DS_Store",
+        "node_modules",
+        "/build",
+        "**/build",
+        "/dist",
+        "**/dist",
+        ".env",
+        ".env.*",
+        "!.env.example",
+        "__generated__",
+        "pnpm-lock.yaml",
+        "package-lock.json",
+        "yarn.lock",
+        "**/coverage"
+    ]
+}
